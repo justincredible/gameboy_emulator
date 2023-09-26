51d17b05a30f7ad2c302f40d8151df521b75f5b0
Justin Symonds <justinjfsymonds@hotmail.com>
2023-09-26 01:01:32 -0700

    Fixed major lag but still not working

--- gameboy_opengl/src/cable_link.rs
+++ gameboy_opengl/src/cable_link.rs
@@ -3,20 +3,21 @@ use gameboy_core::ByteTransfer;
 use std::process::Child;
 use std::sync::atomic::{AtomicU8, Ordering};
 
-use raw_sync::locks::{LockImpl, LockInit, Mutex};
+use raw_sync::locks::{LockGuard, LockImpl, LockInit, Mutex};
 use raw_sync::Timeout;
 use shared_memory::{Shmem, ShmemConf, ShmemError};
 
-pub enum LinkCable {
+pub enum LinkCable<'a> {
     Unlinked,
     Linked {
         owning: Option<Child>,
         shmem: Shmem,
         mutex: (Box<dyn LockImpl>, usize),
+        lock: Option<LockGuard<'a>>,
     },
 }
 
-impl ByteTransfer for LinkCable {
+impl<'a> ByteTransfer for LinkCable<'a> {
 
     fn send(&mut self, data: u8, control: u8) {
         match self {
@@ -64,6 +65,7 @@ impl ByteTransfer for LinkCable {
                     };
 
                     if *status == 2 {
+                        *status = 3;
                         Some((*data, *control))
                     } else {
                         None
@@ -111,12 +113,21 @@ impl ByteTransfer for LinkCable {
 
                     let state = (*ctrl_self, *ctrl_other, *status_self, *status_other);
                     match state {
-                        (0x81, 0x81, 0, 0) | (0x81, 0x80, 0, 0) | (0x80, 0x81, 0, 0) => {
+                        (0x80, 0x80, 0, 0)
+                        | (0x80, 0, 0, 0) | (0, 0x80, 0, 0)
+                        | (0x80, 1, 0, 0) | (1, 0x80, 0, 0) => (),
+                        (0x81, 0x81, 0, 0)
+                        | (0x81, 0x80, 0, 0) | (0x80, 0x81, 0, 0)
+                        | (0x81, 0, 0, 0) | (0, 0x81, 0, 0)
+                        | (0x81, 1, 0, 0) | (1, 0x81, 0, 0) => {
                             println!("Start transfer");
                             *status_self = 1;
                             *status_other = 1;
                         },
-                        (0x81, 0x81, 1, 1) | (0x81, 0x80, 1, 1) | (0x80, 0x81, 1, 1) => {
+                        (0x81, 0x81, 1, 1)
+                        | (0x81, 0x80, 1, 1) | (0x80, 0x81, 1, 1)
+                        | (0x81, 0, 1, 1) | (0, 0x81, 1, 1)
+                        | (0x81, 1, 1, 1) | (1, 0x81, 1, 1) => {
                             println!("Transferring");
                             let temp = *data_self;
                             *data_self = *data_other;
@@ -142,11 +153,11 @@ impl ByteTransfer for LinkCable {
                             *status_self = 0;
                             *status_other = 0;
                         },
-                        (0x81, 0x00, 1, 2) => {
+                        (0x81, 0x00, 1, 2) | (0x81, 0x01, 1, 2) => {
                             println!("Other transfer complete");
                             *status_other = 3;
                         },
-                        (0x00, 0x81, 2, 1) => {
+                        (0x00, 0x81, 2, 1) | (0x01, 0x81, 2, 1) => {
                             println!("Self transfer complete");
                             *status_self = 3;
                         },
@@ -160,6 +171,25 @@ impl ByteTransfer for LinkCable {
                             *status_other = 2;
                             *status_self = 0;
                         },
+                        (_, _, 1, 3) | (_, _, 3, 1) => {
+                            *status_self = 0;
+                            *status_other = 0;
+                        },
+                        (_, _, 0, 2) | (_, _, 2, 0) => (),//*status_other = 3,
+                        //(_, _, 2, 0) => *status_self = 3,
+                        (_, _, 0, 3) => *status_other = 0,
+                        (_, _, 3, 0) => *status_self = 0,
+                        (0xFF, 0xFF, 0xFF, 0xFF) => {
+                            *data_self = 0;
+                            *data_other = 0;
+                            *ctrl_self = 0;
+                            *ctrl_other = 0;
+                            *status_self = 0;
+                            *status_other = 0;
+                        },
+                        (0, 0, 0, 0)
+                        | (0x01, 0, 0, 0) | (0, 0x01, 0, 0)
+                        | (0x7E, 0, 0, 0) | (0, 0x7E, 0, 0) => (),
                         _ => {
                             println!("Unexpected state: {:?}", state);
                         },
@@ -172,16 +202,17 @@ impl ByteTransfer for LinkCable {
     fn ready(&self) -> bool {
         match self {
             LinkCable::Unlinked => false,
-            LinkCable::Linked { mutex, owning, .. } => mutex.0
-                .try_lock(Timeout::Val(std::time::Duration::from_secs(0)))
+            LinkCable::Linked { mutex, lock, owning, .. } => mutex.0
+                .lock()
                 .map_or(false, |guard| {
                     let status = unsafe {
-                        &*(*guard).add(owning
+                        *(*guard).add(owning
                             .as_ref()
                             .map_or(6, |_| 2))
                     };
 
-                    *status == 0 || *status == 3
+                    //*lock = Some(guard);
+                    status == 0 || status == 3
                 }),
         }
     }
@@ -196,13 +227,12 @@ impl ByteTransfer for LinkCable {
 
 const SHM_SZ: usize = 48;
 
-impl From<(bool, Option<Child>)> for LinkCable {
+impl<'a> From<(bool, Option<Child>)> for LinkCable<'a> {
 
     fn from(value: (bool, Option<Child>)) -> Self {
         match value {
             (false, None) => LinkCable::Unlinked,
             (_, value) => {
-                println!("Create shared memory");
                 let shmem = match ShmemConf::new().size(SHM_SZ).flink("link_cable").create() {
                     Ok(m) => m,
                     Err(ShmemError::LinkExists) => ShmemConf::new().flink("link_cable").open().unwrap(),
@@ -230,7 +260,7 @@ impl From<(bool, Option<Child>)> for LinkCable {
                     };
                     {
                         let guard = mutex.0.lock().unwrap();
-                        for i in 0..6 {
+                        for i in 0..8 {
                             unsafe { *(*guard).add(i) = 0xFF; }
                         }
                     }
@@ -248,12 +278,11 @@ impl From<(bool, Option<Child>)> for LinkCable {
                     }
                 };
 
-                println!("Shared memory complete");
-
                 LinkCable::Linked {
                     owning: value,
                     shmem,
                     mutex,
+                    lock: None,
                 }
             },
         }
